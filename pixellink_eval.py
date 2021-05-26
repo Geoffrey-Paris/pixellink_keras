3557d1438e06be26f63c128579d6fe5d736545f7
Geoffrey Paris <geoffrey.paris@insa-rouen.fr>
2021-05-26 21:18:55 +0200

    Test avec partition

--- pixellink_eval.py
+++ pixellink_eval.py
@@ -7,52 +7,56 @@
 import os
 import numpy as np
 import cv2
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+import matplotlib.pyplot as plt
 import imutils
+
 from pixellink_model import create_pixellink_model
 from pixellink_utils import *
 
-import matplotlib.pyplot as plt
+if __name__ == "__main__":
+    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
-r_mean = 123.
-g_mean = 117.
-b_mean = 104.
-rgb_mean = [r_mean, g_mean, b_mean]
-
-
-img_path = r'./samples/img_1099.jpg'
-save_weights = r'./weights/pixellink.h5'
-model = create_pixellink_model(acf='relu')
-model.load_weights(save_weights)
-
-image = cv2.imread(img_path)
-image, *r = resize_image(image)
-image_ori = image.copy()
-image = image[...,::-1] - rgb_mean
-image = np.expand_dims(image, axis=0)
-
-pixel_pos_scores, link_pos_scores = model.predict(image)
-pixel_pos_scores = softmax(pixel_pos_scores, axis=-1)
-link_pos_scores_reshaped = link_pos_scores.reshape(link_pos_scores.shape[:-1]+(8, 2))
-link_pos_scores = softmax(link_pos_scores_reshaped, axis=-1)
-
-masks = decode_batch(pixel_pos_scores, link_pos_scores, pixel_conf_threshold=0.75, link_conf_threshold=0.9)
-
-bboxes = mask_to_bboxes(masks[0], image_ori.shape)
+    # r_mean = 123.
+    # g_mean = 117.
+    # b_mean = 104.
+    # rgb_mean = [r_mean, g_mean, b_mean]
 
 
-image_c = image_ori.copy()
-for box in bboxes:
-    points = np.reshape(box, [4, 2])
-    cv2.line(image_c,tuple(points[0]),tuple(points[1]),(0,0,255),2)
-    cv2.line(image_c,tuple(points[0]),tuple(points[3]),(0,0,255),2)
-    cv2.line(image_c,tuple(points[1]),tuple(points[2]),(0,0,255),2)
-    cv2.line(image_c,tuple(points[2]),tuple(points[3]),(0,0,255),2)
+    # img_path = r'./samples/img_1099.jpg'
+    img_path = r'./dataset/MyDrive/dataset/train/102593-1.png'
+    save_weights = r'./weights/pixellink.h5'
+    model = create_pixellink_model(acf='relu')
+    model.load_weights(save_weights)
 
-plt.imshow(image_c)
+    image = cv2.imread(img_path)
+    image, *r = resize_image(image)
+    image_ori = image.copy()
+    # image = image[...,::-1] - rgb_mean
+    image = np.expand_dims(image, axis=0)
 
-#cv2.imshow('image', image_c)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+    pixel_pos_scores, link_pos_scores = model.predict(image)
+    pixel_pos_scores = softmax(pixel_pos_scores, axis=-1)
+    link_pos_scores_reshaped = link_pos_scores.reshape(link_pos_scores.shape[:-1]+(8, 2))
+    link_pos_scores = softmax(link_pos_scores_reshaped, axis=-1)
 
-cv2.imwrite('img_1099_res.jpg', image_c)
+    masks = decode_batch(pixel_pos_scores, link_pos_scores, pixel_conf_threshold=0.75, link_conf_threshold=0.9)
+
+    bboxes = mask_to_bboxes(masks[0], image_ori.shape)
+
+
+    image_c = image_ori.copy()
+    for box in bboxes:
+        points = np.reshape(box, [4, 2])
+        cv2.line(image_c,tuple(points[0]),tuple(points[1]),(0,0,255),2)
+        cv2.line(image_c,tuple(points[0]),tuple(points[3]),(0,0,255),2)
+        cv2.line(image_c,tuple(points[1]),tuple(points[2]),(0,0,255),2)
+        cv2.line(image_c,tuple(points[2]),tuple(points[3]),(0,0,255),2)
+
+    plt.imshow(image_c)
+
+    #cv2.imshow('image', image_c)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+    # cv2.imwrite('img_1099_res.jpg', image_c)
+    cv2.imwrite('img_test_partition.jpg', image_c)
